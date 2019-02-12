24865545628aaa12d70cf9141989157f220f3d2b
Akio Tanaka <aqua.ciel.et.neige@gmail.com>
2019-02-12 23:25:16 +0900

    [WIP] 計算機整備中

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
   var summary = document.getElementById('summary');
+  var single_mp = document.getElementById('single_mp');
   var rotation = document.getElementById('rotation');
   var unlimited = document.getElementById('unlimited');
 
@@ -11,8 +12,21 @@ document.addEventListener('DOMContentLoaded', function () {
   var lose_un_num = document.getElementById('lose_un_num');
   var win_un_avg = document.getElementById('win_un_avg');
 
+  var single_radio = document.getElementById('single');
+  var double_radio = document.getElementById('double');
+
+  var single_mode = document.getElementById('single_mode');
+  var double_mode = document.getElementById('double_mode');
+  var single_mode_display = document.getElementById('single_mode_display');
+  var double_mode_display = document.getElementById('double_mode_display');
+
   Math.trunc = Math.trunc || function (x) {
     return x < 0 ? Math.ceil(x) : Math.floor(x);
+  }
+
+  var init = function () {
+    double_mode.style.display = "none";
+    double_mode_display.style.display = "none";
   }
 
   int = parseInt;
@@ -21,12 +35,12 @@ document.addEventListener('DOMContentLoaded', function () {
     // ローテ、アンリミを加味した計算
     if (rotation.value != 0 && unlimited.value != 0) {
 
-    // ローテーションだけで計算
+      // ローテーションだけで計算
     } else if (rotation.value != 0 && unlimited.value == 0) {
       win_num.innerText = Math.trunc(summary.value / 100);
       lose_num.innerText = Math.trunc((summary.value - rotation.value) / 100);
       win_avg.innerText = (int(win_num.innerText) / (int(win_num.innerText) + int(lose_num.innerText)) * 100).toFixed(4);
-    // アンリミだけで計算
+      // アンリミだけで計算
     } else if (rotation.value == 0 && unlimited.value != 0) {
       win_un_num.innerText = Math.trunc(summary.value / 100);
       lose_un_num.innerText = Math.trunc((summary.value - unlimited.value) / 100);
@@ -46,4 +60,19 @@ document.addEventListener('DOMContentLoaded', function () {
     calc_win_avg();
   }, false);
 
+  single_radio.addEventListener('click', function () {
+    single_mode.style.display = "block";
+    double_mode.style.display = "none";
+    single_mode_display.style.display = "block";
+    double_mode_display.style.display = "none";
+  }, false);
+
+  double_radio.addEventListener('click', function () {
+    single_mode.style.display = "none";
+    double_mode.style.display = "block";
+    single_mode_display.style.display = "none";
+    double_mode_display.style.display = "block";
+  }, false);
+
+  init();
 })
